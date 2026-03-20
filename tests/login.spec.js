5ae69c0182cb883ffe0eaac2cb4b0193873b386c
Rostislav <penshinrost@gmail.com>
2026-03-20 21:06:34 +0300

    rf-describe

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,14 +5,14 @@ import { LoginPageData } from './testData/loginPageData';
 import { InventoryPageData } from './testData/inventoryPageData';
 
 test.describe("TC_01 | Authorization under different users", () => {
+    /** @type {LoginPage} */
+    let loginPage;
     test.beforeEach(async ({ page }) => {
         await page.goto('/');
+        loginPage = new LoginPage(page);
     });
     
-    /* TC_01.001 | Login Functionality > Positive Login Scenarios > Successful Login Standard User */
     test("TC_01.001.01 | Login page elements are displayed correctly", async ({ page }) => {    
-        const loginPage = new LoginPage(page);
-        
         await expect(loginPage.usernameField).toBeVisible();
         await expect(loginPage.passwordField).toBeVisible();
         await expect(loginPage.loginButton).toBeVisible();
@@ -22,7 +22,6 @@ test.describe("TC_01 | Authorization under different users", () => {
     });
 
     test("TC_01.001.02 | Login form accepts input correctly", async ({ page }) => {    
-        const loginPage = new LoginPage(page);
         await loginPage.fillUsername(UserData.standard_user.username);
         await loginPage.fillPassword(UserData.standard_user.password);
 
@@ -31,21 +30,18 @@ test.describe("TC_01 | Authorization under different users", () => {
     });
 
     test("TC_01.001.03 | Password field masks entered text", async ({ page }) => {    
-        const loginPage = new LoginPage(page);
         await loginPage.fillPassword(UserData.standard_user.password);
 
         await expect(loginPage.passwordField).toHaveAttribute('type', 'password');
     });
 
     test("TC_01.001.04 | Authorization under a standard user", async ({ page }) => {    
-        const loginPage = new LoginPage(page);
         await loginPage.submitFormLogin(UserData.standard_user.username, UserData.standard_user.password);
        
         await expect(page).toHaveURL(InventoryPageData.URL);
     });
 
     test("TC_01.001.05 | Product inventory is displayed after successful login", async ({ page }) => {    
-        const loginPage = new LoginPage(page);
         const inventoryPage = await loginPage.submitFormLogin(UserData.standard_user.username, UserData.standard_user.password);
         const allCards = await inventoryPage.productCards.all();
 
@@ -57,16 +53,12 @@ test.describe("TC_01 | Authorization under different users", () => {
     });
 
     test("TC_01.001.06 | No error message appears after successful login", async ({ page }) => {    
-        const loginPage = new LoginPage(page);
         await loginPage.submitFormLogin(UserData.standard_user.username, UserData.standard_user.password);
        
         await expect(loginPage.errorMessage).not.toBeVisible();
     });
-    /*END TC_01.001*/
-
 
     test("TC_01.002 | Invalid Username Login Attempt", async ({ page }) => {    
-        const loginPage = new LoginPage(page);
         await loginPage.submitFormLogin(UserData.invalid_user.username, UserData.standard_user.password);
        
         await expect(loginPage.errorMessage).toBeVisible();
