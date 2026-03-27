b49825a16a62e9533a230e4fdb3f3a6fd28d3e71
gmpero <72013308+gmpero@users.noreply.github.com>
2026-03-27 19:28:32 +0300

    TC_03.001 Product Catalog Display (#42)

--- tests/inventory.spec.js
+++ tests/inventory.spec.js
@@ -1,8 +1,36 @@
-import { test, expect } from "@playwright/test";
-import { LoginPage } from "./pageObject/loginPage";
-import { UserData } from "./testData/userData";
-import { InventoryPageData } from "./testData/inventoryPageData";
-import { InventoryPage } from "./pageObject/inventoryPage";
+import { test, expect } from '@playwright/test';
+import { LoginPage } from './pageObject/loginPage';
+import { UserData } from './testData/userData';
+import { InventoryPageData } from './testData/inventoryPageData';
+import { InventoryAssertions } from './assertions/inventoryAssertions'
+
+test.describe("TC_03 | Product Catalog > Display and Content", () => {
+    let loginPage;
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+        loginPage = new LoginPage(page);
+    });
+
+    test("TC_03.001.01 | Page title is displayed correctly", async ({page}) => {
+        const inventoryPage = await loginPage.submitFormLogin(UserData.standard_user.username, UserData.standard_user.password);
+        await expect(inventoryPage.title).toHaveText(InventoryPageData.TITLE);
+    });
+
+    test("TC_03.001.02 | Correct number of products is displayed", async ({page}) => {
+        const inventoryPage = await loginPage.submitFormLogin(UserData.standard_user.username, UserData.standard_user.password);
+        await expect(inventoryPage.productCards).toHaveCount(InventoryPageData.PRODUCTS_COUNT);
+    });
+
+    test("TC_03.001.03 | All product card elements are visible", async ({page}) => {
+        const inventoryPage = await loginPage.submitFormLogin(UserData.standard_user.username, UserData.standard_user.password);
+        await InventoryAssertions.validateAllProductCards(inventoryPage);
+    });
+
+    test("TC_03.001.04 | Product Catalog Validate Data", async ({page}) => {
+        const inventoryPage = await loginPage.submitFormLogin(UserData.standard_user.username, UserData.standard_user.password);
+        await InventoryAssertions.validateProductCardsData(inventoryPage);
+    });
+});
 
 test.describe("TC_01 | Menu Functionality / Product Catalog", () => {
     /** @type {LoginPage} */
--- tests/pageObject/inventoryPage.js
+++ tests/pageObject/inventoryPage.js
@@ -6,12 +6,34 @@ class InventoryPage {
      */
 
     constructor(page) {
-        this.page = page;
         this.header = new Header(page);
-        this.title = page.getByTestId("title");
-        this.productCards = page.getByTestId("inventory-item");
+        this.page = page;
+		this.title = page.getByTestId('title');
+		this.productCards = page.getByTestId('inventory-item');
         this.mainContent = page.getByTestId("inventory-container");
-    }
+    
+	}
+
+	getProductTitle(card) {
+		return card.getByTestId('inventory-item-name');
+	}
+
+	getProductDescription(card) {
+		return card.getByTestId('inventory-item-desc');
+	}
+
+	getProductPrice(card) {
+		return card.getByTestId('inventory-item-price');
+	}
+
+	getAddToCardButton(card) {
+		return card.locator('.btn');
+	}
+
+	getProductImage(card) {
+		return card.locator('img.inventory_item_img');
+	}
+        
 }
 
 export { InventoryPage };
