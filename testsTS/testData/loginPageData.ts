1a0da8b769641da0eae53cccc433808e14fa20d6
Daria Bannikova <dbdadika@gmail.com>
2026-04-08 13:46:50 -0400

    Add direct URL access protection test and update error notifications

--- testsTS/testData/loginPageData.ts
+++ testsTS/testData/loginPageData.ts
@@ -2,11 +2,14 @@
 
 const LoginPageData = {
     errorNotifications: {
-        missingUsername: 'Epic sadface: Username is required',
-        missingPassword:  'Epic sadface: Password is required',
-        lockedUser : 'Epic sadface: Sorry, this user has been locked out.',
-        invalidCredentials: 'Epic sadface: Username and password do not match any user in this service',
-    }
-}
+        missingUsername: "Epic sadface: Username is required",
+        missingPassword: "Epic sadface: Password is required",
+        lockedUser: "Epic sadface: Sorry, this user has been locked out.",
+        invalidCredentials:
+            "Epic sadface: Username and password do not match any user in this service",
+        accessDenied:
+            "Epic sadface: You can only access '/inventory.html' when you are logged in.",
+    },
+};
 
-export {LoginPageData};
+export { LoginPageData };
